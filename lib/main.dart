4a1caa401a14332f08ce5814fe101ca874cc62c7
HumzaBaig <humzacodemail@gmail.com>
2020-02-17 18:57:57 -0600

    create Quote object along with QuoteCard stateless widget. Then created a function to delete quotes to finalize the projects first draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,43 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
-void main() => runApp();
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: 'Be yourself; everyone else is already taken.', author: 'Oscar Wilde' ),
+    Quote(text: 'So many books, so little time.', author: 'Albert Einstein'),
+    Quote(text: 'A room without books is like a body without a soul.', author: 'Someone Else')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
+      ),
+    );
+  }
+}
